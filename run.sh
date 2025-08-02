69ac09c4dc8b6452173854090b802715c24a24c8
Asuka Minato <i@asukaminato.eu.org>
2025-08-02 20:44:41 +0900

    update script

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 set -eux -o pipefail
 
 readarray -t WEBSITE_NAMES < <(ls css-gen-op/)
-
-WEBSITE_NAME=google cargo run -r --bin naive &> tmp.txt
-WEBSITE_NAME=google cargo run -r --bin bit &> bit_tmp.txt
-WEBSITE_NAME=google cargo run -r --bin tri &> tri_tmp.txt
+export WEBSITE_NAME=google
+cargo run -r --bin naive &> tmp.txt
+cargo run -r --bin bit &> bit_tmp.txt
+cargo run -r --bin tri &> tri_tmp.txt
 difft <(grep '^Descendant' tmp.txt) <(grep '^Descendant' bit_tmp.txt)
 difft <(grep '^Descendant' tmp.txt) <(grep '^Descendant' tri_tmp.txt)
