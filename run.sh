ba638d17432603dc7bc3d14ccc7f87324a0805c5
Asuka Minato <i@asukaminato.eu.org>
2025-08-02 20:33:21 +0900

    add diff script

--- run.sh
+++ run.sh
@@ -6,3 +6,5 @@ readarray -t WEBSITE_NAMES < <(ls css-gen-op/)
 WEBSITE_NAME=google cargo run -r --bin naive &> tmp.txt
 WEBSITE_NAME=google cargo run -r --bin bit &> bit_tmp.txt
 WEBSITE_NAME=google cargo run -r --bin tri &> tri_tmp.txt
+difft <(grep '^Descendant' tmp.txt) <(grep '^Descendant' bit_tmp.txt)
+difft <(grep '^Descendant' tmp.txt) <(grep '^Descendant' tri_tmp.txt)
