ddb18fa5dd249b9aa7a437f780094409eb0bed7d
Asuka Minato <i@asukaminato.eu.org>
2025-08-04 14:30:04 +0900

    fix bit

--- src/bit.rs
+++ src/bit.rs
@@ -177,14 +177,30 @@ impl BitVectorHtmlNode {
                     selectors: parent_selectors.to_vec(),
                 };
                 let &parent_bit_index = state_map.get(&parent_rule).unwrap();
-                //dbg!(input.iter().all(|x|!x));
-                if input[parent_bit_index] {
+                if self.has_ancestor_matching_rule(parent_bit_index, input) {
                     new_state[bit_index] = true;
                 }
             }
         }
-
         new_state
+    }
+    fn has_ancestor_matching_rule(&self, parent_bit_index: usize, input: &[bool]) -> bool {
+        unsafe {
+            let mut current_parent = self.parent;
+            while let Some(parent_ptr) = current_parent {
+                let parent_node = &*parent_ptr;
+                if parent_node
+                    .output_state
+                    .get(parent_bit_index)
+                    .copied()
+                    .unwrap_or(false)
+                {
+                    return true;
+                }
+                current_parent = parent_node.parent;
+            }
+        }
+        false
     }
     fn collect_all_matches(
         &self,
@@ -344,6 +360,7 @@ fn apply_frame(tree: &mut BitVectorHtmlNode, frame: &LayoutFrame, hm: &HashMap<C
             //   dbg!(frame.frame_id, frame.command_name.as_str());
             *tree = tree.json_to_html_node(frame.command_data.get("node").unwrap(), &hm);
             tree.fix_parent_pointers();
+            tree.recompute_styles(hm);
         }
         "add" => {
             //   dbg!(frame.frame_id, frame.command_name.as_str());
