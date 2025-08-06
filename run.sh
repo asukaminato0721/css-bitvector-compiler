e32aa719f2ab37f66dde2b9cf782c02b95f99004
Asuka Minato <i@asukaminato.eu.org>
2025-08-06 21:33:45 +0900

    check all three websites (#34)
    * check all three websites
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -2,9 +2,12 @@
 set -eux -o pipefail
 
 readarray -t WEBSITE_NAMES < <(ls css-gen-op/)
-export WEBSITE_NAME=google
-cargo run -r --bin naive &> tmp.txt
-cargo run -r --bin bit &> bit_tmp.txt
-cargo run -r --bin tri &> tri_tmp.txt
-diff <(grep '^Descendant' tmp.txt | cut -d' ' -f2-) <(grep '^Done' bit_tmp.txt | cut -d' ' -f2-)
-diff <(grep '^Descendant' tmp.txt) <(grep '^Descendant' tri_tmp.txt)
+
+for name in "${WEBSITE_NAMES[@]}"; do
+    export WEBSITE_NAME=$name
+    cargo run -r --bin naive &> tmp.txt
+    cargo run -r --bin bit &> bit_tmp.txt
+    cargo run -r --bin tri &> tri_tmp.txt
+    diff <(grep '^Descendant' tmp.txt | cut -d' ' -f2-) <(grep '^Done' bit_tmp.txt | cut -d' ' -f2-)
+    diff <(grep '^Descendant' tmp.txt) <(grep '^Descendant' tri_tmp.txt)
+done
